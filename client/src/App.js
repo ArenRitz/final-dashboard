59633af20f6891b54b4a1adc513c4fbdc4555b63
ArenRitz <aren.dolbakian@gmail.com>
2022-09-02 18:48:57 -0400

    Add bookmark components

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
+import BookmarkCategory from "./components/BookmarkCategory";
+
 
 function App() {
   return <div className="h-screen w-screen">
     <div className="h-1/4 w-1/4">
     <button onClick={() => console.log('hello')} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"> Button </button>
     </div>
-
-
+   
+    <BookmarkCategory />
 
   </div>;
 }
