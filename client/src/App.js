3fda8ccf176620bdbc202cb6ac02dc6539890e72
ArenRitz <aren.dolbakian@gmail.com>
2022-09-02 13:00:51 -0400

    Initial client setup

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return <div className="h-screen w-screen">
+    <div className="h-1/4 w-1/4">
+    <button onClick={() => console.log('hello')} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"> Button </button>
+    </div>
+
+
+
+  </div>;
+}
+
+export default App;
