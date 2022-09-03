8ea45df311ef42891deb4c7b9216fd03a83a0742
MT91-dev <tariqm1991@gmail.com>
2022-09-03 19:32:03 +0000

    Add horoscope component and render to App

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,5 @@
+import React from 'react';
+import Aztro from './components/horoscopeWidget';
 import "./App.css";
 
 function App() {
@@ -6,6 +8,9 @@ function App() {
     <button onClick={() => console.log('hello')} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"> Button </button>
     </div>
 
+  <div>
+    <Aztro />
+  </div>
 
 
   </div>;
