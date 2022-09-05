f3a4a398d0fd8a8db1e11b5401e45bc82174a219
MT91-dev <tariqm1991@gmail.com>
2022-09-05 22:29:31 +0000

    Import spotify widget component to app

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import "./App.css";
 import BookmarkCategory from "./components/BookmarkCategory";
 import Weather from "./components/Weather";
 import TwitchWidgetList from "./components/TwitchWidgetList";
+import WidgetSpotify from './components/WidgetSpotify';
 
 function App() {
   return (
@@ -22,6 +23,8 @@ function App() {
       <BookmarkCategory />
 
       <Aztro />
+
+      <WidgetSpotify />
 
       <div className="h-1/4 w-1/4">
         <button onClick={() => console.log('hello')} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"> Button </button>
