17c9e9a8f216db2966991218860fd90830e9bef1
A-DUYVESTYN <a.duyvestyn@gmail.com>
2022-09-06 17:08:19 +0000

    install @react-google-maps/api

---
+++ client/src/components/Maps.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { GoogleMap } from "@react-google-maps/api"
+
+
+export default function Maps() {
+
+  return (
+    <div>
+      Map Placeholder
+      {/* <GoogleMap defaultZoom={10} defaultCenter={{ lat:45.50, lon:-73.57 }} /> */}
+
+    </div>
+  );
+
+}
