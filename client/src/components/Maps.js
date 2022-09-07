b13a2cba6dbe8577baf0a1e7c4ff779a22bd1664
ArenRitz <aren.dolbakian@gmail.com>
2022-09-07 15:32:18 -0400

    Add hide/show features to maps widget

--- client/src/components/Maps.js
+++ client/src/components/Maps.js
@@ -3,11 +3,12 @@
 // disable adblockers
 import React, { useRef, useState } from "react";
 import { GoogleMap, useJsApiLoader, Marker, Autocomplete, DirectionsRenderer } from "@react-google-maps/api"
+import Button from "./Button";
 
 const userCentre = { lat: 43.647725, lng: -79.384851 }
 const userRoute = {origin:"Waterloo, ON", destination:"Toronto, ON"}
 
-export default function Maps() {
+export default function Maps(props) {
   const [map, setMap] = useState( /** @type google.maps.Map */(null)) //add autocompletions provided by google maps
   const [directionsResponse, setDirectionsResponse] = useState(null)
   const [distance, setDistance] = useState(null)
@@ -73,6 +74,8 @@ export default function Maps() {
     return <div>Maps Loading...</div>
   }
   return (
+    <>
+    <Button type="hide" click={props.click} name="Maps" />
     <div className='flex flex-col justify-between w-fit figma-bookmark-container px-5 py-5'>
       <section>
         <Autocomplete>
@@ -82,10 +85,10 @@ export default function Maps() {
           <input className="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 px-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="Destination" type="text" name="destination" ref={destinationRef} />
         </Autocomplete>
         <div className='flex'>
-          <button className="text-slate-400 block bg-white rounded-md py-2 px-2 mr-2 rounded hover:outline-none hover:border-sky-500 hover:ring-sky-500 hover:ring-1" onClick={calculateRoute}>Show Route</button>
-          <button className="text-slate-400 block bg-white rounded-md py-2 px-2 mr-2 rounded hover:outline-none hover:border-sky-500 hover:ring-sky-500 hover:ring-1" onClick={clearRoute}>Clear</button>
+          <button className="text-slate-400 block bg-white rounded-md py-2 px-2 mr-2 hover:outline-none hover:border-sky-500 hover:ring-sky-500 hover:ring-1" onClick={calculateRoute}>Show Route</button>
+          <button className="text-slate-400 block bg-white rounded-md py-2 px-2 mr-2 hover:outline-none hover:border-sky-500 hover:ring-sky-500 hover:ring-1" onClick={clearRoute}>Clear</button>
           <button
-            className="text-slate-400 block bg-white rounded-md py-2 px-2 mr-2 rounded hover:outline-none hover:border-sky-500 hover:ring-sky-500 hover:ring-1"
+            className="text-slate-400 block bg-white rounded-md py-2 px-2 mr-2 hover:outline-none hover:border-sky-500 hover:ring-sky-500 hover:ring-1"
             name="center-back"
             onClick={() => map.panTo(userCentre)}
           >
@@ -119,6 +122,7 @@ export default function Maps() {
         {directionsResponse && <DirectionsRenderer directions={directionsResponse} />}
       </GoogleMap>
     </div>
+    </>
   );
 
 }
