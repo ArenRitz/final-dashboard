98d9fbe75249f1a45cc94a541a198c160014d29d
MT91-dev <tariqm1991@gmail.com>
2022-09-07 01:20:15 +0000

    Require axios, cors, spotify-web-api-node and initialize spotify router

--- server/app.js
+++ server/app.js
@@ -1,13 +1,19 @@
 var express = require('express');
+const axios = require('axios');
+const cors = require('cors')
+const SpotifyWebApi = require('spotify-web-api-node');
+var router = express.Router();
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const spotifyRouter = require('./routes/spotify');
 
 var app = express();
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -16,5 +22,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/spotify', spotifyRouter);
+
+console.log("I am running on backend server before token refresh request");
 
 module.exports = app;
