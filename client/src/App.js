270f0708663129ce58723cb41d9082302bf3994f
ArenRitz <aren.dolbakian@gmail.com>
2022-09-08 12:45:08 -0400

    Add login component and render widgets based on loggedIn status

--- client/src/App.js
+++ client/src/App.js
@@ -10,8 +10,9 @@ import TwitchWidgetList from "./components/TwitchWidgetList";
 import WidgetSpotifyList from "./components/WidgetSpotifyList";
 import Settings from "./components/Settings";
 import Button from "./components/Button";
-import Maps from './components/Maps';
+import Maps from "./components/Maps";
 
+import Login from "./components/Login";
 
 function App() {
   const [theme, setTheme] = useState("dark");
@@ -37,65 +38,76 @@ function App() {
     }));
   };
 
-
   //handle theme change via drop down menu
   const handleThemeChange = (e) => {
     const { value } = e.target;
     setTheme(value);
   };
 
+  const [loggedInStatus, setLoggedInStatus] = useState(false);
+
   return (
     <div className="App" data-theme={theme}>
-      {show.Aztro ? <Aztro click={hideComponenet} /> : null}
-      <br></br>
+      {loggedInStatus && (
+        <>
+          {show.Aztro && <Aztro click={hideComponenet} />}
+          <br></br>
 
-      {show.Twitch ? (
-        <TwitchWidgetList click={hideComponenet} showBool={show.Twitch} />
-      ) : null}
-      <br></br>
+          {show.Twitch && (
+            <TwitchWidgetList click={hideComponenet} showBool={show.Twitch} />
+          )}
+          <br></br>
 
-      {show.Recipe ? (
-        <WidgetRecipe click={hideComponenet} showBool={show.Recipe} />
-      ) : null}
-      <br></br>
+          {show.Recipe && (
+            <WidgetRecipe click={hideComponenet} showBool={show.Recipe} />
+          )}
+          <br></br>
 
-      {show.Clock ? (
-        <Clock click={hideComponenet} showBool={show.Clock} />
-      ) : null}
-      <br></br>
+          {show.Clock && <Clock click={hideComponenet} showBool={show.Clock} />}
+          <br></br>
 
-      {show.Bookmarks ? (
-        <BookmarkCategory click={hideComponenet} showBool={show.Bookmarks} />
-      ) : null}
-      <br></br>
+          {show.Bookmarks && (
+            <BookmarkCategory
+              click={hideComponenet}
+              showBool={show.Bookmarks}
+            />
+          )}
+          <br></br>
 
-      {show.Weather ? (
-        <WeatherCustom click={hideComponenet} showBool={show.Weather} />
-      ) : null}
-      <br></br>
+          {show.Weather && (
+            <WeatherCustom click={hideComponenet} showBool={show.Weather} />
+          )}
+          <br></br>
 
+          {show.Spotify && (
+            <WidgetSpotifyList click={hideComponenet} showBool={show.Spotify} />
+          )}
+          <br></br>
 
-      {show.Spotify ? (
-        <WidgetSpotifyList click={hideComponenet} showBool={show.Spotify} />
-      ) : null}
-      <br></br>
+          {show.Maps && <Maps click={hideComponenet} showBool={show.Maps} />}
 
-      {show.Maps ? (
-      <Maps click={hideComponenet} showBool={show.Maps} />
-      ) : null}
+          {show.Settings && (
+            <Settings
+              click={hideComponenet}
+              themeChange={handleThemeChange}
+              theme={theme}
+              showBools={show}
+            />
+          )}
 
-
-
-      {show.Settings ? (
-        <Settings click={hideComponenet} themeChange={handleThemeChange} theme={theme} showBools={show} />
-      ) : null}
-
-      <div className="fixed top-1/3 right-0 h-1/3 w-1/6 group">
-        <div className="bg-slate-500 fixed top-1/2 -right-8 h-20 w-8 rounded-l-2xl flex flex-col justify-around invisible transition transform group-hover:visible group-hover:transform group-hover:transition-all group-hover:-translate-x-8">
-          <Button type="settings" click={hideComponenet} name="Settings" />
-          <Button type="edit" click={hideComponenet} name="Edit" />
-        </div>
-      </div>
+          <div className="fixed top-1/3 right-0 h-1/3 w-1/6 group">
+            <div className="bg-slate-500 fixed top-1/2 -right-8 h-20 w-8 rounded-l-2xl flex flex-col justify-around invisible transition transform group-hover:visible group-hover:transform group-hover:transition-all group-hover:-translate-x-8">
+              <Button type="settings" click={hideComponenet} name="Settings" />
+              <Button type="edit" click={hideComponenet} name="Edit" />
+            </div>
+          </div>
+        </>
+      )} 
+       {!loggedInStatus && (
+        <>
+          <Login></Login>
+        </>
+      )}
     </div>
   );
 }
