898eb628d576d07448756af88ae1f9d2d3fa1d34
MT91-dev <tariqm1991@gmail.com>
2022-09-08 20:41:53 +0000

    Fix comment

--- server/routes/horoscopes.js
+++ server/routes/horoscopes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const horoscopes = require('../db/queries/horoscopes');
 
-/* GET users listing. */
+/* GET users horoscope from db */
 router.get('/:id', (req, res) => {
   horoscopes.getHoroscopeByUserId(req.params.id).then(data => {
     res.send({ data });
