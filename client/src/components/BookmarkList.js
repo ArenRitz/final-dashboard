6af84e099b68cca0ab270d28be63295afdf8c6d8
ArenRitz <aren.dolbakian@gmail.com>
2022-09-11 14:25:45 -0400

    Fix phantom bookmark showing when no bookmarks are in a category

--- client/src/components/BookmarkList.js
+++ client/src/components/BookmarkList.js
@@ -10,9 +10,6 @@ const BookmarkList = (props) => {
   const [showNewBookmark, setShowNewBookmark] = useState(false);
   const [editCatMode, setEditCatMode] = useState(false);
 
-
-
-    
   // toggle edit category mode
   const toggleEditCatMode = () => {
     setEditCatMode(!editCatMode);
@@ -23,19 +20,16 @@ const BookmarkList = (props) => {
     setShowEdit(!showEdit);
   };
 
-
-
-
-
-
-
   let bookmarkList = bookmarksItems.map((bookmark, index) => {
     return (
       <>
-      <div  key={index} className="relative w-full">
-            <div className="my-1 bg-slate-400/20 rounded-full shadow-md shadow-black/50 group overflow-hidden w-full" onMouseEnter={toggleEdit}>
+        {bookmark.title && (
+          <div key={index} className="relative w-full">
+            <div
+              className="my-1 bg-slate-400/20 rounded-full shadow-md shadow-black/50 group overflow-hidden w-full"
+              onMouseEnter={toggleEdit}
+            >
               <BookmarkItem
-            
                 title={bookmark.title}
                 URL={bookmark.url}
                 mode={props.mode}
@@ -44,36 +38,39 @@ const BookmarkList = (props) => {
                 categoryID={bookmark.categoryID}
                 deleteSingle={props.deleteSingle}
                 editSingle={props.editSingle}
-
               />
-
             </div>
-            </div>
-
+          </div>
+        )}
       </>
     );
   });
 
-
   return (
     <>
       <div className="flex flex-col items-center justify-center relative">
-      {props.mode === "edit" && (
-        <>
-        <button className="btn btn-primary btn-sm absolute -top-8 -left-0 z-20" onClick={toggleEditCatMode}> EDIT </button>
-        {editCatMode && (
-          <NewCategory 
-          type="editCat"
-          userID={props.id}
-          categoryID={props.categoryID}
-          editCategory={props.editCategory}
-          category={props.category}
-          toggleEditCatMode={toggleEditCatMode}
-          />
+        {props.mode === "edit" && (
+          <>
+            <button
+              className="btn btn-primary btn-sm absolute -top-8 -left-0 z-20"
+              onClick={toggleEditCatMode}
+            >
+              {" "}
+              EDIT{" "}
+            </button>
+            {editCatMode && (
+              <NewCategory
+                type="editCat"
+                userID={props.id}
+                categoryID={props.categoryID}
+                editCategory={props.editCategory}
+                category={props.category}
+                toggleEditCatMode={toggleEditCatMode}
+              />
+            )}
+          </>
         )}
-        </>
-      )}
-      
+
         {bookmarkList}
 
         {props.mode === "edit" && (
