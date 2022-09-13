55701e6aa3f3cd6a0aebb097f2e48d8b0a28843c
ArenRitz <aren.dolbakian@gmail.com>
2022-09-13 14:29:51 -0400

    Styling fix for edit mode

--- client/src/components/WidgetTime.js
+++ client/src/components/WidgetTime.js
@@ -16,11 +16,22 @@ const NewClock = (props) => {
   };
 
   return (
-    <div className="text-5xl bg-base-200 clock ml-16 pt-[12px] border-2 border-base-content rounded-3xl whitespace-nowrap w-fit pr-6">
+    <div className="text-5xl bg-base-200 clock ml-16 pt-[12px] border-2 border-base-content rounded-3xl whitespace-nowrap w-fit pr-6 h-26">
+
+      <Clock
+        format={'h:mm:ss A'}
+        ticking={true}
+        timezone={timezone}
+      />
+      {props.mode === "view" && (
+      <div className="timezone-text pl-4 h-6" >
+        Timezone: {timezone}
+      </div>
+       )}
       {props.mode === "edit" && (
-        <div className="flex timezone-dropdown justify-center">
+        <div className="flex timezone-dropdown justify-center h-6">
           <select
-            className="text-center select select-bordered w-full max-w-xs justify-center"
+            className="text-center select select-bordered w-[200px] select-xs justify-center"
             onChange={pickTimezone}
           >
             <option disabled selected>
@@ -32,14 +43,6 @@ const NewClock = (props) => {
           </select>
         </div>
       )}
-      <Clock
-        format={'h:mm:ss A'}
-        ticking={true}
-        timezone={timezone}
-      />
-      <div className="timezone-text pl-4">
-        Timezone: {timezone}
-      </div>
     </div>
   )
 }
