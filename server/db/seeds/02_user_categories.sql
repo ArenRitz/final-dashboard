ea64d81fce86bd66dbd0ee89e2b3939f5aeeb0f9
MT91-dev <tariqm1991@gmail.com>
2022-09-13 20:43:52 +0000

    Update user seed information

--- server/db/seeds/02_user_categories.sql
+++ server/db/seeds/02_user_categories.sql
@@ -1,14 +1,14 @@
-INSERT INTO users_categories (user_id, category_name) VALUES (1, 'Coding');
-INSERT INTO users_categories (user_id, category_name) VALUES (1, 'Gaming');
+INSERT INTO users_categories (user_id, category_name) VALUES (1, 'Work');
+INSERT INTO users_categories (user_id, category_name) VALUES (1, 'Social');
 INSERT INTO users_categories (user_id, category_name) VALUES (1, 'Reading');
 INSERT INTO users_categories (user_id, category_name) VALUES (1, 'Watching');
 
-INSERT INTO users_categories (user_id, category_name) VALUES (2, 'Work');
-INSERT INTO users_categories (user_id, category_name) VALUES (2, 'Fun');
+INSERT INTO users_categories (user_id, category_name) VALUES (2, 'Shopping');
+INSERT INTO users_categories (user_id, category_name) VALUES (2, 'Music');
 INSERT INTO users_categories (user_id, category_name) VALUES (2, 'Cooking');
 INSERT INTO users_categories (user_id, category_name) VALUES (2, 'Drawing');
 
-INSERT INTO users_categories (user_id, category_name) VALUES (3, 'Music');
+INSERT INTO users_categories (user_id, category_name) VALUES (3, 'Fun');
 INSERT INTO users_categories (user_id, category_name) VALUES (3, 'Reading');
 INSERT INTO users_categories (user_id, category_name) VALUES (3, 'Coding');
 INSERT INTO users_categories (user_id, category_name) VALUES (3, 'School');
