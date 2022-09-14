5aab137737e5db21b27e94b2e70f997fae2b38c7
Adam <a.duyvestyn@gmail.com>
2022-09-14 18:56:13 -0400

    console error cleanup

--- client/src/components/WeatherCustom.js
+++ client/src/components/WeatherCustom.js
@@ -1,19 +1,18 @@
 // Open Weather API: https://openweathermap.org/current
-
 import axios from "axios"
 import { useState, useEffect } from "react";
 import React from "react";
-import Button from "./Button";
 
-export default function WeatherCustom(props) {
+export default function WeatherCustom() {
 
   const [coords, setCoords] = useState({ lat: 43.26, lon: -80.21 })
   const [isLoading, setIsLoading] = useState(false);
   const [weatherData, setWeatherData] = useState({
     temp: null,
     city_name: null,
-    weather: { description: null, icon: null },
+    weather: { description: null, icon: null, iconUrl: null },
   })
+
 
   useEffect(() => {
     const weatherParams = {
@@ -24,9 +23,6 @@ export default function WeatherCustom(props) {
       lon: coords.lon,
       lat: coords.lat,
     };
-
-    // AbortController prevents multiple useEffect calls with UseStrict
-    const controller = new AbortController()
 
     navigator.geolocation.getCurrentPosition(res => {
       const latitude = res.coords.latitude
@@ -40,13 +36,16 @@ export default function WeatherCustom(props) {
 
       const url = `https://api.openweathermap.org/data/2.5/weather?lat=${weatherParams.lat}&lon=${weatherParams.lon}&appid=${weatherParams.key}&units=metric`
       setIsLoading(true);
-      return axios.get(url, { signal: controller.signal }) //use signal to prevent multiple useEffect calls with UseStrict
+      return axios.get(url) 
         .then((res) => {
           // console.log("openweather response:", res.data)
           const responseData = {
             temp: Math.round(res.data.main.temp),
             city_name: res.data.name,
-            weather: { description: res.data.weather[0].description.split(' ').map((word) => word.charAt(0).toUpperCase() + word.slice(1)).join(' '), icon: res.data.weather[0].icon }
+            weather: { 
+              description: res.data.weather[0].description.split(' ').map((word) => word.charAt(0).toUpperCase() + word.slice(1)).join(' '), 
+              icon: res.data.weather[0].icon, 
+              iconUrl: `http://openweathermap.org/img/w/${res.data.weather[0].icon}.png`}
           }
 
           setWeatherData(() => responseData)
@@ -57,7 +56,6 @@ export default function WeatherCustom(props) {
           // console.log(msg, err)
         })
     })
-    return (() => controller.abort()) //to prevent multiple useEffect calls with UseStrict
 
   }, [coords.lon, coords.lat])
 
@@ -91,7 +89,7 @@ export default function WeatherCustom(props) {
               <h1 className=""> {weatherData.weather.description}</h1>
             </div>
             <div className=" w-[40%] flex justify-center items-center">
-              <img alt="icon" className=" w-[4rem]" src={`http://openweathermap.org/img/w/${weatherData.weather.icon}.png`} />
+              <img alt="icon" className=" w-[4rem]" src={weatherData.weather.iconUrl} />
             </div>
           </div>
         }
